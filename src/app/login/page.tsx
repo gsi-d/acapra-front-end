248fad2b73a6b7e0e5550f4cdb14a06ac640cddf
Guilherme Molverstet <guilhermemolver12@gmail.com>
2025-05-13 20:30:06 -0300

    subindoLogin

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,15 @@
-import ComboBox from "../components/ComboBox";
+import { Login } from "@mui/icons-material";
+import React, { useState } from "react";
 
-export default function Page() {
-  return (
-        <ComboBox/>
-  );
+export default function LoginPage() {
+  const [gmail, setGmail] = useState("");
+  const [senha, setSenha] = useState("");
+  const [lembreMe, setlembreMe] = useState(false);
+
+  const login = (e:any) => {
+    e.preventDefault();
+    console.log({ gmail, senha, lembreMe });
+  };
+
+  return 
 }
