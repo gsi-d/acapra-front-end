9998d9930e0e4cd8982dcf17b219556e0c787a59
Luiz Guilherme Martins Dos Santos <luiz.beedbr@gmail.com>
2025-05-12 23:24:14 -0300

    login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,34 @@
 import HelloWorld from "../components/HelloWorld";
+import './page.css';
 
 export default function Page() {
   return (
-        <HelloWorld/>
+    <div className="informacao">
+      <div className="mensagem">
+        <h2>Oi, Bem vindo de volta!</h2>
+        <p>Você está em uma boa companhia</p>
+      </div>
+
+      <div className="login-direito">
+        <div className="login-conteudo">
+          <h1>Entrar</h1>
+          <p>Novo Usuário? <a href="#">Crie uma conta</a></p>
+
+          <form id="login-email">
+            <label htmlFor="email">Entre com seu e-mail</label>
+            <input type="email" id="email" required />
+            <input type="password" id="senha" required />
+            <div className="opcoes">
+              <label>
+                <input type="checkbox" id="esquece" /> Lembre de mim
+              </label>
+              <a href="#" className="esqueceu">Esqueceu a senha?</a>
+            </div>
+
+            <button type="submit">Entrar</button>
+          </form>
+        </div>
+      </div>
+    </div>
   );
 }
